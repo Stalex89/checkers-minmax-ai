10345d66780c44002eb1ffb11516d93326be9904
Stalex89 <23526237+Stalex89@users.noreply.github.com>
2018-06-03 22:27:15 +0200

    refactor UI agent for computer versus computer matches

--- AI_Lab3/Agent.cpp
+++ AI_Lab3/Agent.cpp
@@ -4,7 +4,7 @@
 #include "Board.h"
 
 
-Agent::Agent(int depth, Board &board) : m_depth(depth)
+Agent::Agent(int depth, Board &board, Piece::Color agentColor) : m_depth(depth), m_agentColor(agentColor)
 {
 	// iitialize root node
 	m_root = new Node(m_root, board);
@@ -22,25 +22,121 @@ std::pair<int, std::vector<std::pair<int, std::string>>> Agent::alphaBeta(Node *
 {
 	Piece::Color currColor; 
 
-	if (isPlayerMaximize)
+	if (isPlayerMaximize && m_agentColor == Piece::Color::WHITE)
 	{
 		node->getNodeBoard().clearAllMovesForColor(Piece::Color::BLACK);
 		currColor = Piece::Color::WHITE;
 	}
-	else
+	else if (isPlayerMaximize && m_agentColor == Piece::Color::BLACK)
 	{
 		node->getNodeBoard().clearAllMovesForColor(Piece::Color::WHITE);
 		currColor = Piece::Color::BLACK;
 	}
+	else if (!isPlayerMaximize && m_agentColor == Piece::Color::WHITE)
+	{
+		node->getNodeBoard().clearAllMovesForColor(Piece::Color::WHITE);
+		currColor = Piece::Color::BLACK;
+	}
+	else if (!isPlayerMaximize && m_agentColor == Piece::Color::BLACK)
+	{
+		node->getNodeBoard().clearAllMovesForColor(Piece::Color::BLACK);
+		currColor = Piece::Color::WHITE;
+	}
+
 
 	node->getNodeBoard().clearAllMovesForColor(currColor);
 	node->getNodeBoard().calculateAllMovesForColor(currColor);
+
+
+//	// terminal condition
+//	if (depth == 0 || node->getNodeBoard().isGameOver(currColor))
+//	{
+//		std::vector<std::pair<int, std::string>> po = node->getNodeBoard().getPositions();
+//		int eb = evaluateBoard(node->getNodeBoard(), isPlayerMaximize);
+//		std::pair<int, std::vector<std::pair<int, std::string>>> pair = std::make_pair(eb, po);
+//		return pair;
+//	}
+//	// traverse through all pieces on the board
+//	for (int idx = 0; idx < node->getNodeBoard().getPieceSize(); idx++)
+//	{
+//		Piece p = node->getNodeBoard().getPieceByIdx(idx);
+//		
+//			//for maximizer and white piece
+//			if (isPlayerMaximize && p.getColor() == Piece::Color::WHITE && p.hasPossibleMoves())
+//			{
+//				for (auto move : p.getAllPossibleMoves())
+//				{
+//					//create a child node
+//					Node *child = new Node(node, node->getNodeBoard());
+//					
+//						
+//					//update the positions of the child node with new move and update the state of the board
+//					std::vector<std::pair<int, std::string>> pos = node->getNodeBoard().getPositions();
+//					pos.push_back(move);
+//					child->getNodeBoard().setPositions(pos);
+//
+//					std::pair<int, std::vector<std::pair<int, std::string>>> val = alphaBeta(child, depth - 1, false);
+//						
+//					// delete child after evaluation of child node
+//					delete child;
+//					child = nullptr;
+//
+//					// replase aplha if the value of child node is bigger than parent node
+//					node->setAlphaPos((node->getAlphaPos().first > val.first) ? node->getAlphaPos() : val);
+//				
+//					// if alpha is greater or equal beta, we can prune
+//					if (node->getAlphaPos().first >= node->getBetaPos().first)
+//						break;
+//				
+//				
+//				}
+//				
+//				return node->getAlphaPos();
+//			}
+//
+//			else if (!isPlayerMaximize && p.getColor() == Piece::Color::BLACK && p.hasPossibleMoves())
+//			{
+//
+//				for (auto move : p.getAllPossibleMoves())
+//				{
+//					//create a child node
+//					Node *child = new Node(node, node->getNodeBoard());
+//
+//					//update the positions of the child node with new move and update the state of the board
+//					std::vector<std::pair<int, std::string>> pos = node->getNodeBoard().getPositions();
+//					pos.push_back(move);
+//					child->getNodeBoard().setPositions(pos);
+//
+//					std::pair<int, std::vector<std::pair<int, std::string>>> val = alphaBeta(child, depth - 1, true);
+//
+//					// delete child after evaluation of child node
+//					delete child;
+//					child = nullptr;
+//
+//					// replase beta if the value of child node is lesser than parent node
+//					node->setBetaPos((node->getBetaPos().first < val.first) ? node->getBetaPos() : val);
+//
+//					// if alpha is greater or equal beta, we can prune
+//					if (node->getAlphaPos().first >= node->getBetaPos().first)
+//						break;
+//
+//
+//				}
+//
+//					return node->getBetaPos();
+//
+//			}
+//		
+//
+//	}
+//
+//}
 
 	// terminal condition
 	if (depth == 0 || node->getNodeBoard().isGameOver(currColor))
 	{
 		std::vector<std::pair<int, std::string>> po = node->getNodeBoard().getPositions();
-		int eb = evaluateBoard(node->getNodeBoard(), isPlayerMaximize);
+		int eb = evaluateBoard(node->getNodeBoard(), isPlayerMaximize, m_agentColor);
 		std::pair<int, std::vector<std::pair<int, std::string>>> pair = std::make_pair(eb, po);
 		return pair;
 	}
@@ -48,73 +144,73 @@ std::pair<int, std::vector<std::pair<int, std::string>>> Agent::alphaBeta(Node *
 	for (int idx = 0; idx < node->getNodeBoard().getPieceSize(); idx++)
 	{
 		Piece p = node->getNodeBoard().getPieceByIdx(idx);
-		
-			//for maximizer and white piece
-			if (isPlayerMaximize && p.getColor() == Piece::Color::WHITE && p.hasPossibleMoves())
+
+		//for maximizer and white piece
+		if (isPlayerMaximize && p.getColor() == m_agentColor && p.hasPossibleMoves())
+		{
+			for (auto move : p.getAllPossibleMoves())
 			{
-				for (auto move : p.getAllPossibleMoves())
-				{
-					//create a child node
-					Node *child = new Node(node, node->getNodeBoard());
-					
-						
-					//update the positions of the child node with new move and update the state of the board
-					std::vector<std::pair<int, std::string>> pos = node->getNodeBoard().getPositions();
-					pos.push_back(move);
-					child->getNodeBoard().setPositions(pos);
+				//create a child node
+				Node *child = new Node(node, node->getNodeBoard());
 
-					std::pair<int, std::vector<std::pair<int, std::string>>> val = alphaBeta(child, depth - 1, false);
-						
-					// delete child after evaluation of child node
-					delete child;
-					child = nullptr;
 
-					// replase aplha if the value of child node is bigger than parent node
-					node->setAlphaPos((node->getAlphaPos().first > val.first) ? node->getAlphaPos() : val);
-				
-					// if alpha is greater or equal beta, we can prune
-					if (node->getAlphaPos().first >= node->getBetaPos().first)
-						break;
-				
-				
-				}
-				
-				return node->getAlphaPos();
+				//update the positions of the child node with new move and update the state of the board
+				std::vector<std::pair<int, std::string>> pos = node->getNodeBoard().getPositions();
+				pos.push_back(move);
+				child->getNodeBoard().setPositions(pos);
+
+				std::pair<int, std::vector<std::pair<int, std::string>>> val = alphaBeta(child, depth - 1, false);
+
+				// delete child after evaluation of child node
+				delete child;
+				child = nullptr;
+
+				// replase aplha if the value of child node is bigger than parent node
+				node->setAlphaPos((node->getAlphaPos().first > val.first) ? node->getAlphaPos() : val);
+
+				// if alpha is greater or equal beta, we can prune
+				if (node->getAlphaPos().first >= node->getBetaPos().first)
+					break;
+
+
 			}
 
-			else if (!isPlayerMaximize && p.getColor() == Piece::Color::BLACK && p.hasPossibleMoves())
+			return node->getAlphaPos();
+		}
+
+		else if (!isPlayerMaximize && p.getColor() != m_agentColor && p.hasPossibleMoves())
+		{
+
+			for (auto move : p.getAllPossibleMoves())
 			{
+				//create a child node
+				Node *child = new Node(node, node->getNodeBoard());
 
-				for (auto move : p.getAllPossibleMoves())
-				{
-					//create a child node
-					Node *child = new Node(node, node->getNodeBoard());
+				//update the positions of the child node with new move and update the state of the board
+				std::vector<std::pair<int, std::string>> pos = node->getNodeBoard().getPositions();
+				pos.push_back(move);
+				child->getNodeBoard().setPositions(pos);
 
-					//update the positions of the child node with new move and update the state of the board
-					std::vector<std::pair<int, std::string>> pos = node->getNodeBoard().getPositions();
-					pos.push_back(move);
-					child->getNodeBoard().setPositions(pos);
+				std::pair<int, std::vector<std::pair<int, std::string>>> val = alphaBeta(child, depth - 1, true);
 
-					std::pair<int, std::vector<std::pair<int, std::string>>> val = alphaBeta(child, depth - 1, false);
+				// delete child after evaluation of child node
+				delete child;
+				child = nullptr;
 
-					// delete child after evaluation of child node
-					delete child;
-					child = nullptr;
+				// replase beta if the value of child node is lesser than parent node
+				node->setBetaPos((node->getBetaPos().first < val.first) ? node->getBetaPos() : val);
 
-					// replase beta if the value of child node is lesser than parent node
-					node->setBetaPos((node->getBetaPos().first < val.first) ? node->getBetaPos() : val);
+				// if alpha is greater or equal beta, we can prune
+				if (node->getAlphaPos().first >= node->getBetaPos().first)
+					break;
 
-					// if alpha is greater or equal beta, we can prune
-					if (node->getAlphaPos().first >= node->getBetaPos().first)
-						break;
-
-
-				}
-
-					return node->getBetaPos();
 
 			}
-		
+
+			return node->getBetaPos();
+
+		}
+
 
 	}
 
@@ -123,21 +219,21 @@ std::pair<int, std::vector<std::pair<int, std::string>>> Agent::alphaBeta(Node *
 
 
 
-int Agent::evaluateBoard(Board &board, bool isPlayerMaximize)
+int Agent::evaluateBoard(Board &board, bool isPlayerMaximize, Piece::Color agentColor)
 {
 	int value = 0;
 	for (int idx = 0; idx < board.getPiecesNum(); idx++)
 	{
 		Piece p = board.getPieceByIdx(idx);
 		if (p.getPosition() != sf::Vector2f(-100.0f, -100.0f))
-			if (p.getColor() == Piece::Color::WHITE)
+			if (p.getColor() == agentColor)
 			{
 				if (p.isKing())
 					value += (isPlayerMaximize) ? 2 : -2;
 				else
 					value += (isPlayerMaximize) ? 1 : -1;
 			}
-			else if (p.getColor() == Piece::Color::BLACK)
+			else if (p.getColor() != agentColor)
 			{
 				if (p.isKing())
 					value += (isPlayerMaximize) ? -2 : 2;
@@ -149,3 +245,32 @@ int Agent::evaluateBoard(Board &board, bool isPlayerMaximize)
 
 	return value;
 }
+
+
+
+//int Agent::evaluateBoard(Board &board, bool isPlayerMaximize)
+//{
+//	int value = 0;
+//	for (int idx = 0; idx < board.getPiecesNum(); idx++)
+//	{
+//		Piece p = board.getPieceByIdx(idx);
+//		if (p.getPosition() != sf::Vector2f(-100.0f, -100.0f))
+//			if (p.getColor() == Piece::Color::WHITE)
+//			{
+//				if (p.isKing())
+//					value += (isPlayerMaximize) ? 2 : -2;
+//				else
+//					value += (isPlayerMaximize) ? 1 : -1;
+//			}
+//			else if (p.getColor() == Piece::Color::BLACK)
+//			{
+//				if (p.isKing())
+//					value += (isPlayerMaximize) ? -2 : 2;
+//				else
+//					value += (isPlayerMaximize) ? -1 : 1;
+//			}
+//
+//	}
+//
+//	return value;
+//}
--- AI_Lab3/Agent.h
+++ AI_Lab3/Agent.h
@@ -10,19 +10,21 @@ class Agent
 {
 	int m_depth;
 	Node *m_root;
+	Piece::Color m_agentColor;
 
 public:
 
-	Agent(int depth, Board &board);
+	Agent(int depth, Board &board, Piece::Color agentColor);
 	~Agent();
 
 	Node* getRootNode() const { return m_root; }
 	int getTreeDepth() const { return m_depth; }
+	Piece::Color getAgentColor() const { return m_agentColor; }
 
 
 	std::pair<int, std::vector<std::pair<int, std::string>>> alphaBeta(Node *node, int depth, bool isPlayerMaximize);
 
-	int evaluateBoard(Board &board, bool isPlayerMaximize);
+	int evaluateBoard(Board &board, bool isPlayerMaximize, Piece::Color agentColor);
 
 
 };
